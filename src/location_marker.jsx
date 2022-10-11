4d82a095726534c92044ad35ffa16c427ff7d17a
moezAhmad <wyellow19@gmail.com>
2022-10-11 21:52:09 +0500

    Added Polygon layer using markers

--- src/location_marker.jsx
+++ src/location_marker.jsx
@@ -1,23 +1,45 @@
 import React, { useState } from "react";
-import { Marker, useMapEvents } from "react-leaflet";
+import { Marker, Polygon, Popup, useMapEvents } from "react-leaflet";
 
 const LocationMarker = () => {
   const [markers, setMarkers] = useState([]);
+
   // eslint-disable-next-line no-unused-vars
   const map = useMapEvents({
     click: (e) => {
-      setMarkers((prevMarkers) => [...prevMarkers, e]);
+      setMarkers((prevMarkers) => [
+        ...prevMarkers,
+        [e.latlng.lat, e.latlng.lng],
+      ]);
     },
-    // locationfound: (e) => {
-    //   setPosition(e.latlng);
-    //   // map.flyTo(e.latlng, map.getZoom());
-    // },
   });
-
-  return markers.length < 1
-    ? null
-    : markers.map((marker) => (
-        <Marker key={`${marker.latlng}`} position={marker.latlng} />
-      ));
+  const removeMarker = (pos) => {
+    setMarkers((prevMarkers) =>
+      prevMarkers.filter(
+        (prevCoord) => JSON.stringify(prevCoord) !== JSON.stringify(pos)
+        // or (coord) => coord.lat !== pos.lat && coord.lng !== pos.lng
+      )
+    );
+  };
+  return markers.length < 1 ? null : (
+    <>
+      {markers.map((marker) => (
+        <Marker
+          key={`${marker}`}
+          position={marker}
+          eventHandlers={{
+            click: () => {
+              removeMarker(marker);
+            },
+          }}
+        >
+          <Popup>
+            <button type="button">Remove marker</button>
+          </Popup>
+        </Marker>
+      ))}
+      <Polygon positions={markers} />
+    </>
+  );
 };
 export default LocationMarker;
